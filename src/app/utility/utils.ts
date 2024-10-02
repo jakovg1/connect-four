bdc13e6f8279e672f8b156f92f723621448037ae
Jakov <jakov.gracin1@gmail.com>
2024-10-02 13:23:48 +0200

    Fix random int generator bug, add docs

--- src/app/utility/utils.ts
+++ src/app/utility/utils.ts
@@ -1,3 +1,9 @@
-export function getRandomNumberInRange(min: number, max: number): number {
-  return min + Math.round(Math.random() * (max - min));
+/**
+ * Returns a random integer between min and max (min is inclusive, max is exclusive)
+ * @param min
+ * @param max
+ * @returns random integer between min and max
+ */
+export function getRandomIntInRange(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
