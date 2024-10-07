a603ed55ca65c2cd183ccca0b661ebc1491ccba3
Jakov <jakov.gracin1@gmail.com>
2024-10-07 18:54:00 +0200

    Delete unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BoardComponent } from './components/board/board.component';
-import { MainMenuComponent } from './menu/main-menu/main-menu.component';
-import { SettingsMenuComponent } from './menu/settings-menu/settings-menu.component';
 import { ToastModule } from 'primeng/toast';
 import { appRoutingModule } from './app.routes';
 import { MessageService } from 'primeng/api';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoardComponent,
-    MainMenuComponent,
-    SettingsMenuComponent,
-  ],
+  declarations: [AppComponent, BoardComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
